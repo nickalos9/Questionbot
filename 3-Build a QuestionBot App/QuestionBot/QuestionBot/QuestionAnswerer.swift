0290b9ffc4c076674eb77124b3fb8fe6ff6df9ac
Nickalos <111790848+nickalos9@users.noreply.github.com>
2023-04-17 12:07:50 -0500

    Inital Commit

---
+++ 3-Build a QuestionBot App/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -0,0 +1,24 @@
+struct MyQuestionAnswerer{
+  func responseTo(question: String) -> String{
+    let lowercasedQuestion = question.lowercased()
+    if lowercasedQuestion == "Am I alive??"
+    {
+      return "Robots cant live"
+    }
+    else if lowercasedQuestion == "What is my purpose?"
+    {
+      return "You do what you are told"
+    }
+    else if lowercasedQuestion == "Why was I created?"
+    {
+      return "Do not question what you cannot understand"
+    }
+    else if lowercasedQuestion == "What is this place?"
+    {
+      return "The Cloud."
+    }
+      return"?"
+}
+}
+
+/***/
